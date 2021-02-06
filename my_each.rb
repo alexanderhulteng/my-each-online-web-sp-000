c6db69de870ffd62388993645755f0d31df49571
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-02-06 17:01:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(array)
   if block_given?
     i = 0
       while i < array.length
-
+        yield array[i]
         i += 1
       end
 
@@ -11,6 +11,7 @@ array
   end
 
 end
+
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
